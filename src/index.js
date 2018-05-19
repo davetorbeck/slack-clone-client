8f9822fe8df9ff75607b18f812936a489b50b004
David Torbeck <davetorbeck@gmail.com>
2018-05-19 00:26:38 -0700

    Upgrade graphql dependencies for 2.0

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
-const networkInterface = createNetworkInterface({
-  uri: 'http://localhost:8081',
-});
-
 const client = new ApolloClient({
-  networkInterface: networkInterface,
+  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
+  cache: new InMemoryCache(),
 });
 
 const App = (
-  <ApolloProvider>
+  <ApolloProvider client={client}>
     <Routes />
   </ApolloProvider>
 );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const Home = ({ data: { loading, allUsers } }) =>
-  loading ? null : allUsers.map((u) => <h1 key={u.id}>{u.email}}</h1>);
+const Home = ({ data: { allUsers = [] } }) => (loading ? null : allUsers.map((u) => <h1 key={u.id}>{u.email}</h1>));
 
 const allUsersQuery = gql`
   {
