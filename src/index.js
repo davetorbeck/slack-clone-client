df7899734217f4fab91986602c9df4341776ab54
David Torbeck <davetorbeck@gmail.com>
2018-05-28 21:32:06 -0700

    Update apollo client

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'semantic-ui-css/semantic.min.css';
 
-import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8081/graphql' }),
-  cache: new InMemoryCache(),
-});
+import registerServiceWorker from './registerServiceWorker';
+import client from './apollo';
 
 const App = (
   <ApolloProvider client={client}>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
+import CreateTeam from './CreateTeam';
+
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
 );
